88b9cb68625ad79d1e7a6453f5de53caf85f26ed
rodrigo-segui <rodrigo_segui@hotmail.com>
2022-04-06 10:03:43 -0300

    t

--- example-pipelines/example-create/variables.tf
+++ example-pipelines/example-create/variables.tf
@@ -26,21 +26,3 @@ variable "environment" {
   description = "Name of the environment (dev or prod)"
   default     = "dev"
 }
-
-variable "subnet1_region" {
-  type        = string
-  description = "GCP Region where first subnet will be created"
-  default     = "us-central1"
-}
-
-variable "subnet1_zone" {
-  type        = string
-  description = "GCP Zone within Subnet1 Region where GCE instance will be created"
-  default     = "us-central1-a"
-}
-
-variable "subnet1_cidr" {
-  type        = string
-  description = "VPC Network CIDR to be assigned to the VPC being created"
-  default     = "10.0.0.0/17"
-}
